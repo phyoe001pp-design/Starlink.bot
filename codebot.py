a1144343c7df9e04640a821f4b22610e876de67d
phyoe001pp-design <phyoe001.pp@gmail.com>
2026-06-17 09:38:11 +0630

    Update codebot.py

--- codebot.py
+++ codebot.py
@@ -15,7 +15,7 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 # ---------------------- CONFIGURATION ----------------------
-BOT_TOKEN = "8752084120:AAEV7vlqo50srkfhPs-a3xeTfQCrPIHv-3o"
+BOT_TOKEN = "8205038837:AAHkJzpCTsF2aZPg9I4EDOVGgqKiIiZX4f8"
 ADMIN_ID = 6658845504
 DATA_FILE = "bot_data.json"
 
